64fa346ddc62bdb9298de5917d8198a35378a4ae
Danny <dnyshi@Astral>
2023-07-28 09:59:50 -0400

    Add design

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const luke = document.getElementById('one');
+const vader = document.getElementById('two');
+const playerText = document.getElementById('player');
+const botText = document.getElementById('bot');
+const start = document.getElementById('start');
+const mainContainer = document.getElementById('mainContainer');
+
 const gameBoard = (function () {
 
     const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
@@ -11,4 +18,25 @@ const playerFactory = (name, number) => {
 
     number === 1 ? playerOne = name : playerTwo = name;
 }
+
+luke.addEventListener('click', () => {
+    luke.style.border = '5px solid lightgreen';
+    vader.style.border = '5px solid white';
+});
+
+vader.addEventListener('click', () => {
+    vader.style.border = '5px solid lightgreen';
+    luke.style.border = '5px solid white';
+});
+
+start.addEventListener('click', () => {
+    mainContainer.style.display = 'none';
+    const ticBoard = document.createElement('img');
+    const boardContainer = document.createElement('div');
+    boardContainer.id = 'boardContainer';
+    ticBoard.src = 'images/tic-tac.png';
+    ticBoard.id = 'ticBoard';
+    document.body.appendChild(boardContainer);
+    boardContainer.appendChild(ticBoard);
+});
 
